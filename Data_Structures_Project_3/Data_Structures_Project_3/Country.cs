53ce43ffb97761b593bb41d76ca44668ae049a5e
JamesDellAlba <jpdellalba@gmail.com>
2020-03-16 19:44:22 -0400

    day one changes

---
+++ Data_Structures_Project_3/Data_Structures_Project_3/Country.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_Project_3
+{
+    class Country
+    {
+        private String name;
+		private String code;
+		private String capitol;
+		private long population;
+		private long grossDomesticProduct;
+		private int happinessRank;
+		
+		//Constructor
+		public Country(String name, String code, String capitol, long population,
+			long grossDomesticProduct, int happinessRank)
+		{
+			this.name = name;
+			this.code = code;
+			this.capitol = capitol;
+			this.population = population;
+			this.grossDomesticProduct = grossDomesticProduct;
+			this.happinessRank = happinessRank;
+		}
+
+		public String getName()
+		{
+			return this.name;
+		}
+
+		public String getCode()
+		{
+			return this.code;
+		}
+
+		public String getCapitol()
+		{
+			return this.capitol;
+		}
+
+		public long getPopulation()
+		{
+			return this.population;
+		}
+
+		public long getGDP()
+		{
+			return this.grossDomesticProduct;
+		}
+
+		public int getHappinessRank()
+		{
+			return this.happinessRank;
+		}
+
+		public void printCountry()
+		{
+			Console.WriteLine("%-33s %-10s %-20s %-15d %-16d %-10d %-10d\n", this.getName(), this.getCode(), this.getCapitol(),
+					this.getPopulation(), this.getGDP(), this.getHappinessRank());
+		}
+	}
+}
--- Data_Structures_Project_3/Data_Structures_Project_3/Program.cs
+++ Data_Structures_Project_3/Data_Structures_Project_3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Data_Structures_Project_3
 {
@@ -11,8 +12,42 @@ namespace Data_Structures_Project_3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+			Country[] countryArray = new Country[155];
 
-        }
+			try
+			{
+				StreamReader fileOfCountries = new StreamReader(@"C:\Users\jpdel\Documents\Spring 2020 Data Structures Projects\project3-csharp\Data_Structures_Project_3\Data_Structures_Project_3\Countries3.csv");
+				String firstLine = fileOfCountries.ReadLine();
+				String nextline;
+				int i = 0;
+				while ((nextline = fileOfCountries.ReadLine()) != null)
+				{
+					String[] stats = nextline.Split(',');
+					String name = stats[0];
+					String code = stats[1];
+					String capitol = stats[2];
+					long population = Convert.ToInt64(stats[3]);
+					if(!Int64.TryParse(stats[4], out long GDP))
+					{
+						GDP = Convert.ToInt64(stats[4]);
+					}
+					int happiness = Convert.ToInt32(stats[5]);
+					countryArray[i] = new Country(name, code, capitol, population, GDP, happiness);
+					i++;
+				}
+
+			}
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine("File not found");
+			}
+			
+			for (int i = 0; i < 155; i++)
+			{
+				countryArray[i].printCountry();
+			}
+			Console.ReadKey();
+
+		}
     }
 }
