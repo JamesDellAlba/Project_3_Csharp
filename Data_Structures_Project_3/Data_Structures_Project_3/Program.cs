4deba548112dd9194b23bfa5d23d5b9204d9234e
JamesDellAlba <jpdellalba@gmail.com>
2020-03-16 10:36:49 -0400

    initial commit

---
+++ Data_Structures_Project_3/Data_Structures_Project_3/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_Project_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int variable = 32;
+            Console.WriteLine(variable);
+            Console.ReadKey();
+
+        }
+    }
+}
